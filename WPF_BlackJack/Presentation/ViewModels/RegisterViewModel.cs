e5ca37f68d2b5d9616c26f285f1da2c255b5faab
Peter Steele <petersteele111@gmail.com>
2020-03-04 10:40:03 -0500

    Cleaned up RegisterViewModel

--- WPF_BlackJack/Presentation/ViewModels/RegisterViewModel.cs
+++ WPF_BlackJack/Presentation/ViewModels/RegisterViewModel.cs
@@ -8,33 +8,42 @@ namespace WPF_BlackJack.Presentation
 {
     public class RegisterViewModel : ObservableObject
     {
+
+        #region Fields
+
         private GameBusiness _gameBusiness;
         private Player _currentPlayer;
-
+        private string _messages;
         private List<Player> _player = new List<Player>();
+
+        #endregion
+
+        #region Properties
 
         public List<Player> Player
         {
             get => _player;
-            set 
+            set
             {
                 _player = value;
                 OnPropertyChanged(nameof(Player));
             }
         }
 
-        private string _messages;
-
         public string Messages
         {
             get => _messages;
-            set 
+            set
             {
                 _messages = value;
                 OnPropertyChanged(nameof(Messages));
             }
         }
 
+
+        #endregion
+
+        #region Methods
 
         public void Register(string userName)
         {
@@ -59,7 +68,7 @@ namespace WPF_BlackJack.Presentation
         public void Login(string userName)
         {
             Regex rx = new Regex(@"^(?=[a-zA-Z])[-\w.]{0,23}([a-zA-Z\d]|(?<![-.])_)$");
-            bool isValidUserName = rx.IsMatch(userName);
+            bool isValidUserName = rx.IsMatch(userName.ToLower());
             if (!isValidUserName)
             {
                 _messages = "Please enter a Valid Username! No Spaces or Special Characters";
@@ -94,9 +103,16 @@ namespace WPF_BlackJack.Presentation
             }
         }
 
+        #endregion
+
+        #region Constructor
+
         public RegisterViewModel()
         {
 
         }
+
+        #endregion
+       
     }
 }
